b1ec95b7b41380527435af609bf9c9a04c84f12c
Amogh G A <amogh@techmust.com>
2024-10-25 11:55:02 +0530

    Update BackendApplication.java

--- backend/src/main/java/com/docker/backend/BackendApplication.java
+++ backend/src/main/java/com/docker/backend/BackendApplication.java
@@ -13,6 +13,10 @@ public class BackendApplication {
 	public String home() {
         return "Hello, Dockerized World!";
     }
+	@GetMapping("/resp")
+	public String test() {
+        return "returns the response in the /resp api";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
